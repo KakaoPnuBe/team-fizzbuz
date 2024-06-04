707e5e4f1a1f80fd6a3a0b730996274426f7d993
J-1ac <tkdwns26@naver.com>
2024-06-04 20:14:23 +0900

    feat: Print FizzBuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,5 @@
 for i in range(1, 101):              # 1부터 100까지 100번 반복
+    if i % 3 == 0 and i % 5 == 0:    # 3과 5의 공배수일 때
+        print('FizzBuzz')            # FizzBuzz 출력
+        continue
     print(i)                         # 아무것도 해당되지 않을 때 숫자 출력
